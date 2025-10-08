3657d2b75e24c0c3162ca71a3aa1fa72cbca65f0
gustavo <thiagodgomesd892@gmail.com>
2025-10-08 12:21:43 -0300

    Atualizando o banco de dados

--- Script.sql
+++ Script.sql
@@ -3,3 +3,23 @@ drop database if exists dbLogin;
 create database dbLogin;
 
 use dbLogin;
+
+create table tbCliente(
+	Id int primary key auto_increment,
+    Nome varchar(100) not null,
+    Nascimento date not null,
+    Sexo char(1),
+    CPF bigint not null,
+    Telefone bigint,
+    Email varchar(100) not null,
+    Senha varchar(50) not null,
+    Situacao varchar(100) not null
+);
+
+create table tbColaborador(
+	Id int primary key auto_increment,
+    Nome varchar(100) not null,
+    Email varchar(100) not null,
+    Senha varchar(50) not null,
+    Tipo varchar(100) not null
+);
